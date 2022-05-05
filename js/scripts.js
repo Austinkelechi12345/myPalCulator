f9a42f6f0122184619c25d6ecbdad60f6a711316
Austin Kelechi <crossslil59@gmail.com>
2022-05-05 08:42:44 -0700

    i added buttons with several function to carry out a specific calculator task like add,subtract,divide,modulo

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,110 @@
-// // Business Logic
-// function add(number1, number2) {
-//     return number1 + number2;
-// }
-// function subtract(number1, number2) {
-//     return number1 - number2;
-// }
+// Business Logic
+function add(number1, number2, number3) {
+    return number1 + number2 + number3;
 
-// function multiply(number1, number2) {
-//     return number1 * number2;
-// }
-// function divide(number1, number2) {
-//     return number1 / number2;
-// }
+}
+function subtract(number1, number2, number3) {
+    return number1 - number2 - number3;
+}
+function divide(number1, number2, number3) {
+    return number1 / number2 / number3;
+}
+function modulo(number1, number2, number3) {
+    return number1 % number2 % number3;
+}
+function multiply(numb1 , numb2 , numb3){
+    return numb1 * numb2 * numb3;
+}
 
-// // user interface Logic
+// User Interface Logic
+$(document).ready(function () {
+    $("#add").click(function (e) {
+        e.preventDefault();
+        const number1 = parseInt($("#number1").val());
+        const number2 = parseInt($("#number2").val());
+        let number3 = parseInt($("#number3").val());
 
-// $(document).ready(function () {
-//     $("form#calculator").submit(function (event) {
-//         event.preventDefault();
-//         const number1 = parseInt($("#add1").val());
-//         const number2 = parseInt($("#add2").val());
-//         const result = add(number1, number2);
-//         $("#result").text(result);
-//     });
-// });
+        if (isNaN(number3)) {
+            number3 = 0;
+        }
+        const result = add(number1, number2, number3)
+        $("#result").text(result);
+    });
+});
 
-// $(document).ready(function () {
-//     $("form#calculator").submit(function (event) {
-//         event.preventDefault();
-//         const number1 = parseInt($("#subtract1").val());
-//         const number2 = parseInt($("#subtract2").val());
-//         const answer = subtract(number1, number2);
-//         $("#answer").text(answer);
-    
-// });
-// });
+$(document).ready(function () {
+    $("#divide").click(function (e) {
+        e.preventDefault();
+        const number1 = parseInt($("#number1").val());
+        const number2 = parseInt($("#number2").val());
+        let number3 = parseInt($("#number3").val());
+
+        if (isNaN(number3)) {
+            number3 = 1;
+        }
+        const result = divide(number1, number2, number3)
+        $("#result").text(result);
+    });
+});
+
+$(document).ready(function () {
+    $("#multiply").click(function (e) {
+        e.preventDefault();
+        const number1 = parseInt($("#number1").val());
+        const number2 = parseInt($("#number2").val());
+        let number3 = parseInt($("#number3").val());
+
+        if (isNaN(number3)) {
+            number3 = 1;
+        }
+        const result = multiply(number1, number2, number3)
+        $("#result").text(result);
+    });
+});
+
+$(document).ready(function () {
+    $("#modulo").click(function (e) {
+        e.preventDefault();
+        const number1 = parseInt($("#number1").val());
+        const number2 = parseInt($("#number2").val());
+        let number3 = parseInt($("#number3").val());
+
+        // if (isNaN(number3)) {
+        //     number3 = 1;
+        // }
+        const result = modulo(number1, number2, number3)
+        $("#result").text(result);
+    });
+});
+
+$(document).ready(function () {
+    $("#subtract").click(function (e) {
+        e.preventDefault();
+        const number1 = parseInt($("#number1").val());
+        const number2 = parseInt($("#number2").val());
+        let number3 = parseInt($("#number3").val());
+
+        if (isNaN(number3)) {
+            number3 = 0;
+        }
+        const result = subtract(number1, number2, number3)
+        $("#result").text(result);
+    });
+});
 
 
-// $(document).ready(function () {
-//     $("form#calculator").submit(function (event) {
-//         event.preventDefault();
-//         const number1 = parseInt($("#multiply1").val());
-//         const number2 = parseInt($("#multiply2").val());
-//         const resultant = multiply(number1, number2);
-//         $("#after").text(after);
-    
-// });
-// });
 
-// $(document).ready(function () {
-//     $("form#calculator").submit(function (event) {
-//         event.preventDefault();
-//         const number1 = parseInt($("#divide1").val());
-//         const number2 = parseInt($("#divide2").val());
-//         const after = divide(number1, number2);
-//         $("#resultant").text(resultant);
-    
-// });
-// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
